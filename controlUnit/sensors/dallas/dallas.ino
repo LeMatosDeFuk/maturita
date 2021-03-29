dde8f1b7d9ca37fe930386f8d2813c98535da41f
Matěj Černý <mat.cerny3@gmail.com>
2021-03-29 15:04:08 +0200

    opraveni pinu

--- controlUnit/sensors/dallas/dallas.ino
+++ controlUnit/sensors/dallas/dallas.ino
@@ -1,6 +1,6 @@
-int ThermistorPin = A4;
+int ThermistorPin = A1;
 int Vo;
-float R1 = 10000;
+float R1 = 100000;
 float logR2, R2, T, Tc, Tf;
 float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
 
--- controlUnit/uno/uno.ino
+++ controlUnit/uno/uno.ino
@@ -1,40 +1,51 @@
 #include <SoftwareSerial.h>
-SoftwareSerial NodeMCU(12, 13);
+SoftwareSerial NodeMCU(3, 4);
 
-const int humiditySensor1 = 3; // left top
-const int waterPump1 = 5; // left top
+const int humiditySensor1 = 19; // left top
+const int waterPump1 = 5; // right bottom
 
-const int humiditySensor2 = 4; // right bottom
-const int waterPump2 = 6; // right bottom
+const int humiditySensor2 = 18; // right bottom
+const int waterPump2 = 6; // left bottom
 
-const int humiditySensor3 = A1; // left bottom
-const int waterPump3 = 7; // left bottom
+const int humiditySensor3 = 17; // right top
+const int waterPump3 = 7; // left top
 
-const int humiditySensor4 = A0; // right top
+const int humiditySensor4 = 2; // left bottom
 const int waterPump4 = 8; // right top
 
-const int photoResistor = A2;
+const int photoResistor = A0;
 const int waterSensor = A3;
+
+// temperature
+const int temperatureSensor = A1;
+int Vo;
+float R1 = 100000;
+float logR2, R2, T, temperature;
+float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
 
 void setup() {
   Serial.begin(9600);
   NodeMCU.begin(9600);
 
-  //  pinMode(waterPump1, OUTPUT);
-  //  digitalWrite(waterPump1, LOW);
-  //  delay(2000);
-  //  digitalWrite(waterPump1, HIGH);
+//  pinMode(waterPump1, OUTPUT);
+//  digitalWrite(waterPump1, LOW);
+//  delay(2000);
+//  digitalWrite(waterPump1, HIGH);
 
-  //  pinMode(waterPump2, OUTPUT);
-  //  digitalWrite(waterPump2, LOW);
-  //  delay(2000);
-  //  digitalWrite(waterPump2, HIGH);
+//    pinMode(waterPump2, OUTPUT);
+//    digitalWrite(waterPump2, LOW);
+//    delay(2000);
+//    digitalWrite(waterPump2, HIGH);
 
-  //  pinMode(waterPump3, OUTPUT);
-  //  digitalWrite(waterPump3, LOW);
-  //  delay(2000);
-  //  digitalWrite(waterPump3, HIGH);
+//    pinMode(waterPump3, OUTPUT);
+//    digitalWrite(waterPump3, LOW);
+//    delay(2000);
+//    digitalWrite(waterPump3, HIGH);
 
+//    pinMode(waterPump4, OUTPUT);
+//    digitalWrite(waterPump4, LOW);
+//    delay(2000);
+//    digitalWrite(waterPump4, HIGH);
 
   pinMode(humiditySensor1, INPUT);
   pinMode(humiditySensor2, INPUT);
@@ -66,46 +77,50 @@ String getPostData() {
   return "temperature=" + temperature + "&humidity1=" + humidity1  + "&humidity2=" + humidity2  + "&humidity3=" + humidity3  + "&humidity4=" + humidity4 + "&lighting=" + lighting + "&waterLevel=" + waterLevel;
 }
 
-String getLightingValues() {
-  float photoSensorValue = analogRead(photoResistor);
+int convertToLux(int voltage) {
+  float Vout = float(voltage) * (5 / float(1023));
+  float RLDR = (10000 * (5 - Vout));
+  int lux = 500 / (RLDR / 1000);
 
-  return String(photoSensorValue);
+  return lux;
+}
+
+String getLightingValues() {
+  int voltage = analogRead(photoResistor);
+  int lightValue = convertToLux(voltage);
+
+  return String(lightValue);
 }
 
 String getTemperatureValues() {
-  int temperature = 32;
+  Vo = analogRead(temperatureSensor);
+  R2 = R1 * (1023.0 / (float)Vo - 1.0);
+  logR2 = log(R2);
+  T = (1.0 / (c1 + c2 * logR2 + c3 * logR2 * logR2 * logR2));
+  temperature = T - 273.15;
 
   return String(temperature);
 }
 
 String getHumidity1Values() {
-  float humidity = analogRead(humiditySensor1);
-  humidity = 100 - (humidity / 1024) * 100;
-
+  int humidity = digitalRead(humiditySensor1);
   return String(humidity);
 }
 
 
 String getHumidity2Values() {
-  float humidity = analogRead(humiditySensor2);
-  humidity = 100 - (humidity / 1024) * 100;
-
+  int humidity = digitalRead(humiditySensor2);
   return String(humidity);
 }
 
 
 String getHumidity3Values() {
-  float humidity = analogRead(humiditySensor3);
-  humidity = 100 - (humidity / 1024) * 100;
-
+  int humidity = digitalRead(humiditySensor3);
   return String(humidity);
 }
 
-
 String getHumidity4Values() {
-  float humidity = analogRead(humiditySensor4);
-  humidity = 100 - (humidity / 1024) * 100;
-
+  int humidity = digitalRead(humiditySensor4);
   return String(humidity);
 }
 
@@ -122,6 +137,7 @@ void loop() {
   if (NodeMCU.available() > 0) {
     Serial.println(data.c_str());
     NodeMCU.write(data.c_str());
-    delay(1000);
   }
+
+  delay(3000);
 }
